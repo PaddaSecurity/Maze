b5b93af4f2271f76ce5f29df3816fe5b50172a77
PaddaSecurity <50336501+PaddaSecurity@users.noreply.github.com>
2022-04-04 14:39:26 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,4 @@
-// main.cpp
-//
-// ICS 46 Winter 2022
-// Project #1: Dark at the End of the Tunnel
-//
-// This is the program's main() function, which launches the GUI.  You will
-// not want to make any changes to this file.
+
 
 #include "DarkApplication.hpp"
 
